83bcee84921b41147d347e2bd2cff5a42cbd2412
Viktor Oreshkin <imselfish@stek29.rocks>
2018-03-11 22:38:22 +0300

    Fix log_msg usages in TypeReconstructor

--- src/HexRaysCodeXplorer/TypeReconstructor.cpp
+++ src/HexRaysCodeXplorer/TypeReconstructor.cpp
@@ -184,7 +184,7 @@ bool idaapi type_builder_t::check_helper(citem_t *parent, int &off, int &num)
 		if(!strcmp(get_ctype_name(expr_2->x->op), "helper"))
 		{
 			qstring expr;
-			expr_2->x->print1(&expr, NULL);
+			print1hack(expr_2->x, &expr, NULL);
 			tag_remove(&expr);
 
 			if(expr == "LOBYTE")
@@ -326,7 +326,7 @@ bool idaapi type_builder_t::check_ptr(cexpr_t *e, struct_filed &str_fld)
 
 				// get index_value
 				qstring s;
-				expr_2->y->print1(&s, NULL);
+				print1hack(expr_2->y, &s, NULL);
 				tag_remove(&s);
 
 				int base = 10;
@@ -358,7 +358,7 @@ bool idaapi type_builder_t::check_ptr(cexpr_t *e, struct_filed &str_fld)
 			} else if(parent_i->is_expr() && (parent_i->op == cot_asg)) {
 				if (((cexpr_t *)parent_i)->y == e) { //parents[parents.size() - i]) {
 					qstring s;
-					((cexpr_t *)parent_i)->x->print1(&s, NULL);
+					print1hack(((cexpr_t *)parent_i)->x, &s, NULL);
 					tag_remove(&s);
 
 					char comment[258];
@@ -422,7 +422,7 @@ bool idaapi type_builder_t::check_idx(struct_filed &str_fld)
 
 				// get index_value
 				qstring s;
-				expr_2->y->print1(&s, NULL);
+				print1hack(expr_2->y, &s, NULL);
 				tag_remove(&s);
 				int num = atoi(s.c_str());
 
@@ -454,7 +454,7 @@ int idaapi type_builder_t::visit_expr(cexpr_t *e)
 	{
 		// get the variable name
 		qstring s;
-		e->print1(&s, NULL);
+		print1hack(e, &s, NULL);
 		tag_remove(&s);
 
 		// check for the target variable
@@ -586,7 +586,7 @@ bool idaapi reconstruct_type_cb(void *ud)
 			type_builder_t type_bldr;
 			{
 				qstring s;
-				highl_expr->print1(&s, NULL);
+				print1hack(highl_expr, &s, NULL);
 				tag_remove(&s);
 				type_bldr.expression_to_match.insert(s);
 			}
@@ -612,7 +612,7 @@ bool idaapi reconstruct_type_cb(void *ud)
 							if (new_type.print(&type_str, NULL, PRTYPE_DEF | PRTYPE_MULTI))
 							{
 								msg("New type created:\r\n%s\n", type_str.c_str());
-								logmsg(DEBUG, ("New type created:\r\n%s\n", type_str.c_str()));
+								logmsg(DEBUG, "New type created:\r\n%s\n", type_str.c_str());
 								tinfo_t ptype = make_pointer(new_type);
 								vu.set_lvar_type(lvar, ptype);
 								vu.refresh_ctext();
@@ -654,7 +654,7 @@ bool idaapi reconstruct_type(cfuncptr_t cfunc, const qstring& var_name, const qs
 			if(new_type.is_correct()) {
 				qstring type_str = type_name;
 				//if (new_type.print(&type_str, NULL, PRTYPE_DEF | PRTYPE_MULTI))
-				logmsg(DEBUG, ("New type created: %s\n", type_str.c_str()));
+				logmsg(DEBUG, "New type created: %s\n", type_str.c_str());
 
 				bResult = true;
 			}
